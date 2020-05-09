c5a166a4fd85204d59e7298fae487202ca46ae9f
Rohit <B17100@students.iitmandi.ac.in>
2020-05-10 02:54:25 +0530

    Express Js

---
+++ express_server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+    console.log(req.query, req.url  ); 
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
